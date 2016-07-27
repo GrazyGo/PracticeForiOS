5ba67e510bdea7caad101c92d67a1acbcb1821fb
单丹 <shandan@meituan.com>
2016-07-27 16:46:17 +0800

    提交工程代码

---
+++ PhotoWall/PhotoWall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoWall
+//
+//  Created by shandan on 16/7/27.
+//  Copyright © 2016年 shandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
